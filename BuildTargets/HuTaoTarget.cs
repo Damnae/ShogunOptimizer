3ccb54742a6dbe9fc2f1ed25ffd8be26e6e9eccf
Kagetsu <36227892+Kagetsu24@users.noreply.github.com>
2021-09-08 23:34:01 -0300

    Adds Hu Tao

---
+++ BuildTargets/HuTaoTarget.cs
@@ -0,0 +1,74 @@
+﻿using ShogunOptimizer.ArtifactSets;
+using ShogunOptimizer.Characters;
+using ShogunOptimizer.Weapons;
+using System;
+using System.Collections.Generic;
+
+namespace ShogunOptimizer.BuildTargets
+{
+    public class HuTaoTarget : BuildTarget
+    {
+        public override bool UpgradeArtifactsToLvl20 => true;
+        public override string EquippedTo => null;
+        public override bool AllowUnequipped => true;
+        public override bool UseGeneratedArtifacts => false;
+
+        public override Type[] UsefulSets => new Type[]
+        {
+            typeof(GladiatorsFinale),
+            typeof(CrimsonWitchOfFlames),
+        };
+        public override StatType[] UsefulMainStats { get; } = { StatType.HpPercent, StatType.AtkPercent, StatType.PyroDmgBonus, StatType.CritRate, StatType.CritDamage, };
+        public override StatType[] UsefulSubStats { get; } = { StatType.CritDamage, StatType.CritRate, StatType.ElementalMastery, StatType.HpPercent, StatType.AtkPercent, };
+
+        public bool Under50PercentHp = true;
+
+        public override void Initialize(out Character character, out Enemy enemy, out ICollection<Weapon> weapons)
+        {
+
+            character = new HuTao
+            {
+                Level = 90,
+                AttackLevel = 9,
+                SkillLevel = 9,
+                BurstLevel = 9,
+                Constellation = 0,
+                ElementalSkillActive = true,
+                Under50PercentHp = Under50PercentHp,
+            };
+
+            //character.Stats[(int)StatType.BurstDmgBonus] += .003 * 70; // Raiden's E
+
+            //character.Stats[(int)StatType.AtkPercent] += .25; // Pyro resonance
+            //character.Stats[(int)StatType.AtkFlat] += 815; // Bennett
+            //character.Stats[(int)StatType.AtkPercent] += .2; // Bennett's 4 pieces NO
+
+            enemy = new Enemy 
+            { 
+                Level = 80,
+                AffectedBy = Element.Hydro,
+            };
+
+            weapons = new List<Weapon>
+            {
+                new StaffOfHoma(2) { Under50PercentHp = Under50PercentHp, },
+            };
+        }
+
+        public override bool FilterBuild(Build build, Character character, Enemy enemy)
+            => true;
+
+        public override double Evaluate(Build build, Character character, Enemy enemy)
+        {
+            return character.Calculate(HuTao.PropertyCharged, build, HitType.Averaged, enemy);
+        }
+
+        public override void DisplayResults(Build build, Character character, Enemy enemy)
+        {
+            Console.WriteLine($"Pyro DMG Bonus: {character.GetStat(StatType.PyroDmgBonus, build):P}");
+            Console.WriteLine();
+            Console.WriteLine($"Charged Attack: {character.Calculate(HuTao.PropertyCharged, build, HitType.Critical, enemy):#}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Characters/HuTao.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ShogunOptimizer.Characters
+{
+    public class HuTao : Character
+    {
+        public bool ElementalSkillActive;
+        public bool Under50PercentHp;
+
+        public HuTao()
+        {
+            BaseHp = 15552;
+            BaseAtk = 107;
+            BaseDef = 876;
+
+            AscensionStat = .384;
+            AscensionStatType = StatType.CritDamage;
+        }
+
+        public const string PropertyCharged = "charged";
+
+        private readonly double[] ElementalSkillAttackBuff = new[] { .0384, .0407, .0430, .0460, .0483, .0506, .0536, .0566, .0596, .0626, .0656, .0685, .0715 };
+        private readonly double[] ChargedAttackScaling = new[] { 1.3596, 1.4523, 1.545, 1.6686, 1.7613, 1.8695, 2.0085, 2.1476, 2.2866, 2.4257, 2.5647, 2.7038, 2.8428 };
+
+        public override double Calculate(string property, Build build, HitType hitType, Enemy enemy)
+        {
+            var normalAttackElement = ElementalSkillActive ? Element.Pyro : Element.Physical;
+
+            switch(property)
+            {
+                case PropertyCharged: return ChargedAttackScaling[AttackLevel - 1] * GetAtk(build) * GetMultiplier(build, DamageType.Charged, normalAttackElement, hitType, enemy);
+            }
+
+            return base.Calculate(property, build, hitType, enemy);
+        }
+
+        public override double CalculateStat(StatType statType, Build build)
+        {
+            var stat = base.CalculateStat(statType, build);
+
+            switch(statType)
+            {
+                case StatType.AtkFlat when ElementalSkillActive:
+                    stat += Math.Min(4.0 * GetBaseAtk(build), ElementalSkillAttackBuff[SkillLevel - 1] * GetMaxHp(build));
+                    break;
+
+                case StatType.PyroDmgBonus when Under50PercentHp:
+                    stat += .33;
+                    break;
+            }
+
+            return stat;
+        }
+    }
+}
