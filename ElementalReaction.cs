36f8488cfd70b65ee1c8ee49d652b7a57e9202bb
Kagetsu <36227892+Kagetsu24@users.noreply.github.com>
2021-09-08 23:33:17 -0300

    Adds multiplier for amplifying reactions

---
+++ ElementalReaction.cs
@@ -0,0 +1,19 @@
+﻿namespace ShogunOptimizer
+{
+    public enum ElementalReaction
+    {
+        None,
+        Burning,
+        Crystallize,
+        ElectroCharged,
+        Frozen,
+        Melt,
+        ReverseMelt,
+        Overloaded,
+        Shattered,
+        Superconduct,
+        Swirl,
+        Vaporize,
+        ReverseVaporize,
+    }
+}
--- Enemy.cs
+++ Enemy.cs
@@ -3,6 +3,7 @@
     public class Enemy
     {
         public int Level = 90;
+        public Element? AffectedBy = null;
 
         public double[] Resistances = new double[]
         {
